88c9fc80fc2b6423611ad2b3e46c0505bb465caf
Sebastian Gerau <gsebast@web.de>
2015-09-01 21:14:46 +0200

    applied default formatting; zero args length now prints info message instead of failing with ArrayIndexOutOfBoundsException

--- src/main/scala/gs/psa/Psa.scala
+++ src/main/scala/gs/psa/Psa.scala
@@ -7,11 +7,16 @@ import scala.concurrent.duration._
  * @author Sebastian Gerau
  */
 object Psa {
-  def main(args: Array[String]) {
-    val system = ActorSystem("Psa")
-    val urlActor = system.actorOf(UrlActor.props, "urlActor")
-    urlActor ! UrlActor.ScrapeUrl(args(0))
-    system.shutdown()
-    system.awaitTermination(Duration(20, SECONDS))
-  }
+    def main(args: Array[String]) {
+        val system = ActorSystem("Psa")
+        val urlActor = system.actorOf(UrlActor.props, "urlActor")
+
+        args.length match {
+            case 0 => println("No valid URL specified. You must provide at least one valid URL for processing.")
+            case _ => urlActor ! UrlActor.ScrapeUrl(args(0))
+        }
+
+        system.shutdown()
+        system.awaitTermination(Duration(20, SECONDS))
+    }
 }
