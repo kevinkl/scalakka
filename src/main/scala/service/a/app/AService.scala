0698a50615a0755db4279eb3f642d41b6029ec0c
Sebastian Gerau <gsebast@web.de>
2015-12-10 21:44:02 +0100

    Some initial app/service setup

--- src/main/scala/service/a/app/AService.scala
+++ src/main/scala/service/a/app/AService.scala
@@ -1,8 +1,15 @@
 package service.a.app
 
 import service.a.core.ACoreBooted
+import scala.concurrent.ExecutionContext
+import akka.stream.ActorMaterializer
 
 /**
  * @author Sebastian Gerau
  */
-object AService extends App with ACoreBooted
+object AService extends App with ACoreBooted with Config {
+  
+  protected implicit val executor: ExecutionContext = system.dispatcher
+  protected implicit val materializer: ActorMaterializer = ActorMaterializer()
+  
+}
