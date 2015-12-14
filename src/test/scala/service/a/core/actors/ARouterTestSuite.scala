b0fc44485b2a2a8163821f969e76d3aead167f46
Sebastian Gerau <gsebast@web.de>
2015-12-14 21:04:59 +0100

    Added first test for 'ARouter' actor

---
+++ src/test/scala/service/a/core/actors/ARouterTestSuite.scala
@@ -0,0 +1,22 @@
+package service.a.core.actors
+
+import akka.testkit.TestKit
+import org.scalatest.SpecLike
+import akka.testkit.ImplicitSender
+import akka.actor.Props
+import akka.actor.ActorSystem
+
+class ARouterTestSuite extends TestKit(ActorSystem()) with SpecLike with ImplicitSender {
+  import ARouter._
+
+  object `An ARouter` {
+    object `when being send a random message` {
+      def `replies with a "Not yet implemented" message` {
+        val ARouter = system.actorOf(Props[ARouter])
+        aRouter ! "Find the answer to everything"
+        expectMsg("Not yet implemented")
+      }
+    }
+  }
+
+}
