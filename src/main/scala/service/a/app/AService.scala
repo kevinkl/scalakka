2da5567de76daf7a55ea8471c4a426225d85d37e
Sebastian Gerau <gsebast@web.de>
2015-12-17 21:23:30 +0100

    'Hello World' http server implementation

--- src/main/scala/service/a/app/AService.scala
+++ src/main/scala/service/a/app/AService.scala
@@ -1,8 +1,16 @@
 package service.a.app
 
-import service.a.core.ACoreBooted
 import scala.concurrent.ExecutionContext
+import scala.io.StdIn
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
+import akka.http.scaladsl.marshalling.ToResponseMarshallable.apply
+import akka.http.scaladsl.server.Directive.addByNameNullaryApply
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.RouteResult.route2HandlerFlow
 import akka.stream.ActorMaterializer
+import service.a.core.ACoreBooted
 
 /**
  * @author Sebastian Gerau
@@ -11,5 +19,21 @@ object AService extends App with ACoreBooted with AConfig {
   
   protected implicit val executor: ExecutionContext = system.dispatcher
   protected implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val route = path("hello") {
+    get {
+      complete {
+        <h1>Say hello to akka-http</h1>
+      }
+    }
+  }
   
+  val bindingFuture = Http().bindAndHandle(route, "localhost", port.toInt)
+  
+  println(s"Server online at http://localhost:11011/\nPress RETURN to stop...")
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete { _ => system.terminate() }
+
 }
